03c8669aeacc8d638b07491b8212d1e1f0d3acc1
kadiebwe oscar <kadiebweoscar@gmail.com>
2023-12-06 15:07:29 +0100

    refactor all the code on timeline

--- src/components/tweet.jsx
+++ src/components/tweet.jsx
@@ -7,18 +7,18 @@ function Tweet() {
         <>
             {datas.map((data) => (
                 <div className="tweet" key={data.id}>
-                <Avatar avatar={data.tweetProfile} myClassName="tweet-avatar" />
-                <TweetContent
-                    tweetTextValue={data.tweetTextValue}
-                    tweetActionValue1={data.tweetActionValue1}
-                    tweetActionValue2={data.tweetActionValue2}
-                    tweetActionValue3={data.tweetActionValue3}
-                    tweetActionValue4={data.tweetActionValue4}
-                    titleAuthor={data.titleAuthor}
-                    titleAddress={data.titleAddress}
-                    dateHoursPublication={data.dateHoursPublication}
-                    urlImage={data.imageTweet}
-                />
+                    <Avatar avatar={data.tweetProfile} myClassName="tweet-avatar" />
+                    <TweetContent
+                        tweetTextValue={data.tweetTextValue}
+                        tweetActionValue1={data.tweetActionValue1}
+                        tweetActionValue2={data.tweetActionValue2}
+                        tweetActionValue3={data.tweetActionValue3}
+                        tweetActionValue4={data.tweetActionValue4}
+                        titleAuthor={data.titleAuthor}
+                        titleAddress={data.titleAddress}
+                        dateHoursPublication={data.dateHoursPublication}
+                        urlImage={data.imageTweet}
+                    />
                 </div>
             ))}
         </>
