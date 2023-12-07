fd735ed61dd2778f138685caa8b947aa141c1e12
kadiebwe oscar <kadiebweoscar@gmail.com>
2023-12-07 16:21:00 +0100

    add a component whotofollow and a style

--- src/components/whoToFollow.jsx
+++ src/components/whoToFollow.jsx
@@ -1,18 +1,23 @@
 import Avatar from "./avatar"
 import tweetProfil2 from "../images/tweetProfil2.svg"
+import tweetProfilePhoto from "../images/tweet-profile-photo.png"
 import TweetTitleAuthor from "./tweetTitleAuthor"
 import Button from "./button"
 
 export function AccountFollow(props) {
     return(
         <div className="accountFollow">
-            <span>
+            <span className="avatar-and-text">
                 <Avatar  avatar={props.avatar} />
-                <TweetTitleAuthor myStyleTitleAuthor={props.myStyleTitleAuthor} titleAuthor={props.titleAuthor} />
-                <p>@{props.address}</p>
+                <span>
+                    <TweetTitleAuthor 
+                    myStyleTitleAuthor={props.myStyleTitleAuthor} 
+                    titleAuthor={props.titleAuthor} />
+                    <p>@{props.address}</p>
+                </span>
             </span>
-            <span>
-                <Button className="button" textButton="Follow" />
+            <span >
+                <Button className="span-button" textButton="Follow" />
             </span>
         </div>
     )
@@ -25,10 +30,25 @@ function WhoToFollow() {
         <div className="whoToFollow">
             <h2>Who to follow</h2>
             <AccountFollow 
-            avatar={tweetProfil2} 
-            myStyleTitleAuthor="" 
-            titleAuthor="The New York Times" 
-            address="nytimes" />
+                avatar={tweetProfil2} 
+                myStyleTitleAuthor="" 
+                titleAuthor="The New York Times" 
+                address="nytimes" 
+            />
+
+            <AccountFollow 
+                avatar={tweetProfil2} 
+                myStyleTitleAuthor="" 
+                titleAuthor="The New York Times" 
+                address="nytimes" 
+            />
+
+            <AccountFollow 
+                avatar={tweetProfil2} 
+                myStyleTitleAuthor="" 
+                titleAuthor="The New York Times" 
+                address="nytimes" 
+            />
         </div>
     )
 }
