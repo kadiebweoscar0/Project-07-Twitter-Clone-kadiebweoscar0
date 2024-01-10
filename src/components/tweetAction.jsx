408f6c749f4863c22025133a93bf5cb43cd1becf
Kadiebwe <oscar.kadiebwe@kadea.local>
2024-01-10 09:41:53 +0100

    manage over on update action

--- src/components/tweetAction.jsx
+++ src/components/tweetAction.jsx
@@ -92,7 +92,7 @@ function TweetAction(props) {
       onMousseEnter={handleIconHover(3)} 
       onMouseLeave={handleIconHover(3)} 
       onClick={()=>handleLikeClick("update")} 
-      nameIcone={updateIcon} />
+       />
 
     </div>
   );
@@ -205,6 +205,23 @@ export  function Like({value, onMousseEnter, onMouseLeave, onClick, }) {
 }
 
 export  function Update({value, onMousseEnter, onMouseLeave, onClick, nameIcone}) {
+  const [isHover, setIsHover] = useState("#D9D9D9")
+
+  const handleMouseHover = () =>{
+        setIsHover("#c1035b")
+        console.log(isHover);
+  }
+
+  const handleMouseOut = () =>{
+    setIsHover("#D9D9D9")
+  }
+
+
+  const updateIcon =  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M17.53 7.47L12.53 2.47C12.237 2.177 11.762 2.177 11.47 2.47L6.47001 7.47C6.17601 7.763 6.17601 8.238 6.47001 8.53C6.76401 8.822 7.23701 8.824 7.53001 8.53L11.25 4.81V15C11.25 15.414 11.586 15.75 12 15.75C12.414 15.75 12.75 15.414 12.75 15V4.81L16.47 8.53C16.616 8.677 16.808 8.75 17 8.75C17.192 8.75 17.384 8.678 17.53 8.53C17.823 8.237 17.823 7.763 17.53 7.47Z" fill={isHover}/>
+<path d="M19.708 21.944H4.292C3.028 21.944 2 20.916 2 19.652V14C2 13.586 2.336 13.25 2.75 13.25C3.164 13.25 3.5 13.586 3.5 14V19.652C3.5 20.089 3.855 20.444 4.292 20.444H19.708C20.145 20.444 20.5 20.089 20.5 19.652V14C20.5 13.586 20.836 13.25 21.25 13.25C21.664 13.25 22 13.586 22 14V19.652C22 20.916 20.972 21.944 19.708 21.944Z" fill={isHover}/>
+</svg>
+
   return (
     <>
           <span className="tweet-editor-actions hover-of-action" 
@@ -214,9 +231,11 @@ export  function Update({value, onMousseEnter, onMouseLeave, onClick, nameIcone}
           // {handleIconHover(3)} 
           // onMouseLeave= {onMouseLeave} 
           // {handleIconHover(3)}
+          onMouseOver={handleMouseHover}
+          onMouseOut={handleMouseOut}
           >
         <span className="hover-cecle">
-        {nameIcone}
+        {updateIcon}
 </span>
 
         <ValueAction value={value} />
@@ -226,9 +245,6 @@ export  function Update({value, onMousseEnter, onMouseLeave, onClick, nameIcone}
   )
 }
 
-const updateIcon =  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M17.53 7.47L12.53 2.47C12.237 2.177 11.762 2.177 11.47 2.47L6.47001 7.47C6.17601 7.763 6.17601 8.238 6.47001 8.53C6.76401 8.822 7.23701 8.824 7.53001 8.53L11.25 4.81V15C11.25 15.414 11.586 15.75 12 15.75C12.414 15.75 12.75 15.414 12.75 15V4.81L16.47 8.53C16.616 8.677 16.808 8.75 17 8.75C17.192 8.75 17.384 8.678 17.53 8.53C17.823 8.237 17.823 7.763 17.53 7.47Z" fill="#D9D9D9"/>
-<path d="M19.708 21.944H4.292C3.028 21.944 2 20.916 2 19.652V14C2 13.586 2.336 13.25 2.75 13.25C3.164 13.25 3.5 13.586 3.5 14V19.652C3.5 20.089 3.855 20.444 4.292 20.444H19.708C20.145 20.444 20.5 20.089 20.5 19.652V14C20.5 13.586 20.836 13.25 21.25 13.25C21.664 13.25 22 13.586 22 14V19.652C22 20.916 20.972 21.944 19.708 21.944Z" fill="#D9D9D9"/>
-</svg>
+
 
 
