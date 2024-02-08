6f20a067f0e36c750e52a751ed5a3e25f40c7412
kadiebwe oscar <kadiebweoscar@gmail.com>
2024-02-08 19:35:05 +0100

    feat: state management a formulary with api react-hook-form in tweetEditorForm.jsx

--- src/components/tweets/tweetEditorForm.jsx
+++ src/components/tweets/tweetEditorForm.jsx
@@ -3,6 +3,7 @@ import { useContext, useState } from "react";
 import TweetEditorButton from "./tweetEditorButton";
 import TweetEditorInput from "./tweetEditorInput";
 import ContextApp from "../../asset/contextApp";
+import { useForm } from "react-hook-form";
 
 
 let valueTextArea = document.getElementById("kk")
@@ -10,48 +11,72 @@ let valueTextArea = document.getElementById("kk")
 function TweetEditorForm(){
     const {tweets, allDataTweets, setAllDataTweets} = useContext(ContextApp)
 
-    const handlSubmitButton = (e) =>{
-        e.preventDefault();
-    }
+    // const handlSubmitButton = (e) =>{
+    //     e.preventDefault();
+    // }
 
-    const [valueInput, setValueInout] = useState("");
+    // const [valueInput, setValueInout] = useState("");
 
-    const handlChange = (e) =>{
-        const valueI = e.target.value
-        setValueInout(valueI)
-    }
-    const handlClick = (e) =>{
-        e.preventDefault()
-        const copiTweets = tweets.slice()
+    // const handlChange = (e) =>{
+    //     const valueI = e.target.value
+    //     setValueInout(valueI)
+    // }
+    // const handlClick = (e) =>{
+    //     e.preventDefault()
+    //     const copiTweets = tweets.slice()
 
-        tweets.unshift({"id": tweets.length + 1,
-        "tweetProfile": "public/images/profile-photo.png",
-        "titleAuthor": "Bradley Ortiz",
-        "titleAddress":"@Bradley Ortiz",
-        "dateHoursPublication":". 7min",
-        "dateJoined" : "joined December 2023",
-        "following" : 6,
-        "followers": 0,
-        "posts": 0,
-        "cover": "public/images/CoverCUrrentUser.jpeg",
-        "id_tweet" : 1,
-        "tweetTextValue": valueInput,
-        "tweetActionValue1": 0,
-        "tweetActionValue2": 0,
-        "tweetActionValue3": 0,
-        "tweetActionValue4":"",
-        "imageTweet":"",
-    })
+        // tweets.unshift({"id": tweets.length + 1,
+        // "tweetProfile": "public/images/profile-photo.png",
+        // "titleAuthor": "Bradley Ortiz",
+        // "titleAddress":"@Bradley Ortiz",
+        // "dateHoursPublication":". 7min",
+        // "dateJoined" : "joined December 2023",
+        // "following" : 6,
+        // "followers": 0,
+        // "posts": 0,
+        // "cover": "public/images/CoverCUrrentUser.jpeg",
+        // "id_tweet" : 1,
+        // "tweetTextValue": valueInput,
+        // "tweetActionValue1": 0,
+        // "tweetActionValue2": 0,
+        // "tweetActionValue3": 0,
+        // "tweetActionValue4":"",
+        // "imageTweet":"",
+    // })
 
-        setAllDataTweets(copiTweets)
+        const [formData, setFormData] = useState({valueTweet: ""})
+        const {register, formState: {errors}, handleSubmit} = useForm()
+        const onSubmitForm = (data) =>{
+            const copiTweets = tweets.slice()
+            
+            tweets.unshift({"id": tweets.length + 1,
+            "tweetProfile": "public/images/profile-photo.png",
+            "titleAuthor": "Bradley Ortiz",
+            "titleAddress":"@Bradley Ortiz",
+            "dateHoursPublication":". 7min",
+            "dateJoined" : "joined December 2023",
+            "following" : 6,
+            "followers": 0,
+            "posts": 0,
+            "cover": "public/images/CoverCUrrentUser.jpeg",
+            "id_tweet" : 1,
+            "tweetTextValue": data.valueTweet,
+            "tweetActionValue1": 0,
+            "tweetActionValue2": 0,
+            "tweetActionValue3": 0,
+            "tweetActionValue4":"",
+            "imageTweet":"",
+            })
+            setAllDataTweets(copiTweets)
+        }
 
-    }
+    // }
 
     return(
-        <form onSubmit={handlSubmitButton} className="tweet-editor-form">
+        <form onSubmit={handleSubmit(onSubmitForm)} className="tweet-editor-form">
             {/* <TweetEditorInput className="tweet-editor-input" placeholder="what's happening?"/> style={{overflow: "hidden",}} */}
-            <input id="kk" onChange={handlChange}  className="tweet-editor-input" type="text" placeholder="what's happening?"/>
-            <TweetEditorButton onClick={handlClick}  />
+            <input id="kk" className="tweet-editor-input" type="text" placeholder="what's happening?" name="valueTweet" {...register("valueTweet", {required:true})} />
+            <TweetEditorButton />
         </form>
     )
 }
